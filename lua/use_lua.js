034c444b03e7e90a06e666dd5d50157a197d1220
Nerea Rodríguez <alu0101215693@ull.edu.es>
2021-07-05 12:48:20 +0000

    Lua prancticando con nearly

---
+++ lua/use_lua.js
@@ -0,0 +1,22 @@
+const nearley = require("nearley");
+const grammar = require("./lua.js");
+
+const util = require('util');
+const ins = obj => console.log(util.inspect(obj, { depth: null }));
+
+let s = `
+fun (id, idtwo, idthree)  
+  do  
+    do end;
+    do end
+  end 
+end`;
+let ans;
+try {
+    const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+   // Parse something!
+    parser.feed(s);
+    ins(parser.results);
+} catch (e) {
+    console.log(e);
+}
