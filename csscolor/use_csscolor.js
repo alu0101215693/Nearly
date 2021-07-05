b0b9c61df3a25f20a6fa154a2b066bd8ab3de16f
Nerea Rodríguez <alu0101215693@ull.edu.es>
2021-07-05 12:38:29 +0000

    Aprendiendo csscolor con nearly

---
+++ csscolor/use_csscolor.js
@@ -0,0 +1,12 @@
+const nearley = require("nearley");
+const grammar = require("./csscolor.js");
+const util = require("util");
+
+// Create a Parser object from our grammar.
+const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+
+// Parse something!
+parser.feed("#00ff00");
+
+// parser.results is an array of possible parsings.
+console.log(util.inspect(parser.results, {depth: null})); // 
